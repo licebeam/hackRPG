92bcd81c6ae289951668a1c93f0e88fa68eac3a9
Dustin Walker <dustinwalkerart@gmail.com>
2018-09-22 11:03:08 +0900

    changed components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-
+import Desktop from './components/Desktop'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <Desktop />
     );
   }
 }
---
+++ src/components/Desktop.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Desktop extends Component {
+  render() {
+    return (
+      <div className="desktop-container">
+        Check me out
+      </div>
+    );
+  }
+}
+
+export default Desktop;
